7f04d28d858be06cd8fca77ec5134a333b21fc7c
kommwirt <dw@kommunikationsfachwirt.de>
2021-05-31 14:48:04 +0200

    update from input

--- main.js
+++ main.js
@@ -1,17 +1,65 @@
+// String for the result
+
+let allPersons = []
+
+// Get the inputs done
+
+let nameInput = document.getElementById('name')
+let ageInput = document.getElementById('age')
+let examInput = document.getElementById('exam')
+let addInput = document.getElementById('add')
+
+// Get the output done
+
+let errorOutput = document.getElementById('error')
+let listOutput = document.getElementById('output')
+
+
 class Person {
 
-    constructor(nameData, ageData){
+    constructor(nameData, ageData, examData){
         this.name = nameData
         this.age = ageData
+        this.exam = examData
     }
 
-    info(){
-        return `${this.name} is ${this.age} years old`
-    }
+    // info(){
+    //     return `${this.name} is ${this.age} years old`
+    // }
     
 }
 
-let person1 = new Person("Emanuela", 18)
+// let person1 = new Person("Emanuela", 18)
+
+addInput.addEventListener('click', function () {
+
+    if (nameInput.value.length === 0 || ageInput.value.length === 0) {
+        errorOutput.style.display = "block"
+    } else {
+        allPersons.push(new Person(
+            nameInput.value,
+            ageInput.value * 1,
+            examInput.checked
+        ))
+        newListOutput()
+    }
+
+})
+
+function newListOutput() {
+    //reset
+    errorOutput.style.display = "none"
+    listOutput.innerHTML = ''
+
+    //update
+    allPersons.forEach(person => {
+        if (person.exam) {
+            listOutput.innerHTML += `<li>${person.name}, ${person.age} years old.</li>`
+        } else {
+            listOutput.innerHTML += `<li class="red">${person.name}, ${person.age} years old.</li>`
+        }
+    })
+}
 
 
-console.log(person1.info())
+// console.log(person1.info())
